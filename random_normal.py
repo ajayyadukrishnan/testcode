7ab0059e3768dfda8422254ef73893991cc34fed
ajayyadukrishnan <44653928+ajayyadukrishnan@users.noreply.github.com>
2021-11-13 16:50:51 +0530

    Update random_normal.py

--- random_normal.py
+++ random_normal.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 rng = np.random.default_rng()
 vals = rng.standard_normal(1000)
 
+# Set the chart color blue/red/green..etc
+chartColor = "blue"
 #And make a histogram
-plt.hist(vals)
+plt.hist(vals, color=chartColor)
 plt.ylabel('counts')
 plt.title('1000 random numbers generated from a standard normal distribution')
 plt.savefig('standard normal historgram.png')
